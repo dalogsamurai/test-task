1a2f0b287ff7795cf20a3d1e39c95e6fb0eaa0fc
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-10-18 14:17:54 +0300

    catalogue wip

--- starter-boilerplate/src/configs/NavigationConfig.js
+++ starter-boilerplate/src/configs/NavigationConfig.js
@@ -1,5 +1,5 @@
-import { 
-  DashboardOutlined
+import {
+  DashboardOutlined, AppstoreOutlined, MailOutlined, MessageOutlined, CalendarOutlined, BulbOutlined, ShoppingCartOutlined
 } from '@ant-design/icons';
 import { APP_PREFIX_PATH } from 'configs/AppConfig'
 
@@ -12,8 +12,112 @@ const dashBoardNavTree = [{
   submenu: []
 }]
 
+const сatalogueNavTree = [{
+  key: 'Catalogue',
+  path: `${APP_PREFIX_PATH}/сatalogue`,
+  title: 'Catalogue',
+  // title: 'sidenav.apps', 
+  //TODO
+  icon: AppstoreOutlined,
+  breadcrumb: false,
+  submenu: [
+    {
+      key: 'apps-mail',
+      path: `${APP_PREFIX_PATH}/apps/mail/inbox`,
+      title: 'sidenav.apps.mail',
+      icon: MailOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'apps-chat',
+      path: `${APP_PREFIX_PATH}/apps/chat`,
+      title: 'sidenav.apps.chat',
+      icon: MessageOutlined,
+      breadcrumb: false,
+      submenu: []
+    },
+    {
+      key: 'apps-calendar',
+      path: `${APP_PREFIX_PATH}/apps/calendar`,
+      title: 'sidenav.apps.calendar',
+      icon: CalendarOutlined,
+      breadcrumb: true,
+      submenu: []
+    },
+    {
+      key: 'apps-project',
+      path: `${APP_PREFIX_PATH}/apps/project`,
+      title: 'sidenav.apps.project',
+      icon: BulbOutlined,
+      breadcrumb: true,
+      submenu: [
+        {
+          key: 'apps-project-list',
+          path: `${APP_PREFIX_PATH}/apps/project/list`,
+          title: 'sidenav.apps.project.list',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-project-scrumboard',
+          path: `${APP_PREFIX_PATH}/apps/project/scrumboard`,
+          title: 'sidenav.apps.project.scrumboard',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    },
+    {
+      key: 'apps-ecommerce',
+      path: `${APP_PREFIX_PATH}/apps/ecommerce`,
+      title: 'sidenav.apps.ecommerce',
+      icon: ShoppingCartOutlined,
+      breadcrumb: true,
+      submenu: [
+        {
+          key: 'apps-ecommerce-productList',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/product-list`,
+          title: 'sidenav.apps.ecommerce.productList',
+          icon: '',
+          breadcrumb: true,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-addProduct',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/add-product`,
+          title: 'sidenav.apps.ecommerce.addProduct',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-editProduct',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/edit-product/12`,
+          title: 'sidenav.apps.ecommerce.editProduct',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-orders',
+          path: `${APP_PREFIX_PATH}/apps/ecommerce/orders`,
+          title: 'sidenav.apps.ecommerce.orders',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    }
+  ]
+}]
+
+
 const navigationConfig = [
-  ...dashBoardNavTree
+  ...dashBoardNavTree,
+  ...сatalogueNavTree
 ]
 
 export default navigationConfig;
